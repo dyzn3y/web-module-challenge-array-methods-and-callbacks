73b17567006d39fdce42e956aadb632ce066067e
dyzn3y <kdmarshall728@gmail.com>
2023-08-09 16:06:23 -0400

    completed stretch 2 in stretch.js

--- stretch.js
+++ stretch.js
@@ -7,3 +7,26 @@ const totalWCAppearances = (initials) => {
 }
 
 console.log('Total WC Appearances: ', totalWCAppearances('MEX'));
+
+const finalsTie = (data) => {
+    function getFinals(array) {
+        const allFinals = array.filter(function(item){
+            return item.Stage === 'Final';
+        })
+        return allFinals;
+     }
+    const finals = getFinals(data)
+    const finalsWinners = finals.map(item => {
+        if (item['Home Team Goals'] > item ['Away Team Goals']) {
+            return item ['Home Team Name']
+        } else if (item['Home Team Goals'] === item ['Away Team Goals']) {
+            let winCondition = item['Win conditions'].split(' ')
+            return winCondition[0]
+        } else if (item['Home Team Goals'] < item ['Away Team Goals']) {
+            return item['Away Team Name']
+        }
+    });
+    return finalsWinners;
+}
+
+console.log('Stretch 2: ', finalsTie(fifaData));
